0310b1ec0a6f527850177af3725d082d6bc62dd8
Osni Pezzini Junior <osni@sotech.xyz>
2023-02-07 14:45:42 -0300

    Fixed linux namespace

--- Desktop.Linux/Startup.cs
+++ Desktop.Linux/Startup.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Immense.RemoteControl.Desktop.Windows
+namespace Immense.RemoteControl.Desktop.Linux
 {
     public static class Startup
     {
