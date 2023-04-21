f43859b9331c9bf7d466be82c48ef1c32b6fcb21
Jared Goodwin <jared.goodwin@immy.bot>
2023-04-21 06:57:54 -0700

    Put argument processing back into AppState.  Needed for Remotely.

--- Immense.RemoteControl.Desktop.Shared/Services/AppState.cs
+++ Immense.RemoteControl.Desktop.Shared/Services/AppState.cs
@@ -48,14 +48,17 @@ public interface IAppState
 
 public class AppState : IAppState
 {
+    private readonly Dictionary<string, string> _argDict = new();
+    private readonly ILogger<AppState> _logger;
     private readonly IMessenger _messenger;
     private string _host = string.Empty;
 
     private bool _isConfigured;
 
-    public AppState(IMessenger messenger)
+    public AppState(IMessenger messenger, ILogger<AppState> logger)
     {
         _messenger = messenger;
+        _logger = logger;
     }
 
     public event EventHandler<ScreenCastRequest>? ScreenCastRequested;
@@ -66,7 +69,17 @@ public class AppState : IAppState
 
     public string AccessKey { get; private set; } = string.Empty;
 
-    public Dictionary<string, string> ArgDict { get; } = new();
+    public Dictionary<string, string> ArgDict
+    {
+        get
+        {
+            if (!_argDict.Any())
+            {
+                ProcessArgs();
+            }
+            return _argDict;
+        }
+    }
 
 
     public string Host
@@ -139,5 +152,38 @@ public class AppState : IAppState
     public void UpdateHost(string host)
     {
         Host = host;
+    }
+
+    private void ProcessArgs()
+    {
+        var args = Environment.GetCommandLineArgs()
+            .SkipWhile(x => !x.StartsWith("-"))
+            .ToArray();
+
+        for (var i = 0; i < args.Length; i += 2)
+        {
+            try
+            {
+                var key = args[i];
+                if (key != null)
+                {
+                    if (!key.Contains('-'))
+                    {
+                        _logger.LogWarning("Command line arguments are invalid.  Key: {key}", key);
+                        i -= 1;
+                        continue;
+                    }
+
+                    key = key.Trim().TrimStart('-').TrimStart('-').ToLower();
+
+                    ArgDict.Add(key, args[i + 1].Trim());
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while processing args.");
+            }
+
+        }
     }
 }
