4de148bc4f37bf625186155373dd176e6c492207
Jared Goodwin <jared.goodwin@immy.bot>
2023-07-25 07:17:46 -0700

    Add LogResult extension method.

--- Immense.RemoteControl.Shared/Extensions/ILoggerExtensions.cs
+++ Immense.RemoteControl.Shared/Extensions/ILoggerExtensions.cs
@@ -12,9 +12,9 @@ namespace Immense.RemoteControl.Shared.Extensions;
 public static class ILoggerExtensions
 {
     public static IDisposable Enter<T>(
-        this ILogger<T> logger, 
+        this ILogger<T> logger,
         LogLevel logLevel = LogLevel.Debug,
-        [CallerMemberName]string memberName = "")
+        [CallerMemberName] string memberName = "")
     {
         logger.Log(logLevel, "Enter: {name}", memberName);
 
@@ -22,5 +22,53 @@ public static class ILoggerExtensions
         {
             logger.Log(logLevel, "Exit: {name}", memberName);
         });
+    }
+
+    public static void LogResult<T, ResultT>(
+       this ILogger<T> logger,
+       Result<ResultT> result,
+       [CallerMemberName] string callerName = "")
+    {
+        using var logScope = string.IsNullOrWhiteSpace(callerName) ?
+            new NoopDisposable() :
+            logger.BeginScope(callerName);
+
+
+        if (result.IsSuccess)
+        {
+            logger.LogInformation("Successful result.");
+        }
+        else if (result.HadException)
+        {
+            logger.LogError(result.Exception, "Error result.");
+        }
+        else
+        {
+            logger.LogWarning("Failed result. Reason: {reason}", result.Reason);
+        }
+    }
+
+    public static void LogResult<T>(
+      this ILogger<T> logger,
+      Result result,
+      [CallerMemberName] string callerName = "")
+    {
+        using var logScope = string.IsNullOrWhiteSpace(callerName) ?
+            new NoopDisposable() :
+            logger.BeginScope(callerName);
+
+
+        if (result.IsSuccess)
+        {
+            logger.LogInformation("Successful result.");
+        }
+        else if (result.HadException)
+        {
+            logger.LogError(result.Exception, "Error result.");
+        }
+        else
+        {
+            logger.LogWarning("Failed result. Reason: {reason}", result.Reason);
+        }
     }
 }
