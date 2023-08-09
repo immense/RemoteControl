8febc12c005a422202dd3d9ed4264f5b02d880d9
Jared Goodwin <jared.goodwin@immy.bot>
2023-08-09 11:51:40 -0700

    Add IDesktopHubClient.

---
+++ Immense.RemoteControl.Shared/Interfaces/IDesktopHubClient.cs
@@ -0,0 +1,24 @@
+﻿namespace Immense.RemoteControl.Shared.Interfaces;
+
+public interface IDesktopHubClient
+{
+    Task Disconnect(string reason);
+
+    Task GetScreenCast(
+        string viewerId,
+        string requesterName,
+        bool notifyUser,
+        bool enforceAttendedAccess,
+        string organizationName,
+        Guid streamId);
+
+    Task RequestScreenCast(
+        string viewerId,
+        string requesterName,
+        bool notifyUser,
+        Guid streamId);
+
+    Task SendDtoToClient(byte[] dtoWrapper, string viewerConnectionId);
+
+    Task ViewerDisconnected(string viewerId);
+}
