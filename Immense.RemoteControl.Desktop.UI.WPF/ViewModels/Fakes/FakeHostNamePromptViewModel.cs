864b2c18d73704f40dba63a2e17c8d56c4c1d313
Jared Goodwin <jared.goodwin@immy.bot>
2023-08-15 09:09:52 -0700

    Remove WPF project.

--- Immense.RemoteControl.Desktop.UI.WPF/ViewModels/Fakes/FakeHostNamePromptViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Immense.RemoteControl.Desktop.UI.WPF.ViewModels.Fakes;
-
-public class FakeHostNamePromptViewModel : FakeBrandedViewModelBase, IHostNamePromptViewModel
-{
-    public string Host { get; set; } = "https://localhost:7024";
-}
